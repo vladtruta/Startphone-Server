8f4e73340feffdf7e5f8fd195aaa5d81a9e9be85
Vlad Truta <vlad.truta@tapptitude.com>
2020-06-14 18:47:17 +0300

    Small refactor

--- src/persistence/UsersTutorialMissingRequestsEntity.kt
+++ src/persistence/UsersTutorialMissingRequestsEntity.kt
@@ -3,7 +3,7 @@ package com.vladtruta.persistence
 import org.jetbrains.exposed.sql.Table
 
 object UsersTutorialMissingRequestsEntity: Table() {
-    val userId = varchar("userId", 320).references(UserEntity.id)
+    val userId = varchar("userId", 100).references(UserEntity.id)
     val packageName = varchar("packageName", 50).references(ApplicationEntity.packageName)
     val requestCount = integer("requestCount")
 }
--- src/persistence/UsersTutorialsWatchedEntity.kt
+++ src/persistence/UsersTutorialsWatchedEntity.kt
@@ -3,7 +3,7 @@ package com.vladtruta.persistence
 import org.jetbrains.exposed.sql.Table
 
 object UsersTutorialsWatchedEntity: Table() {
-    val userId = varchar("userId", 320).references(UserEntity.id)
+    val userId = varchar("userId", 100).references(UserEntity.id)
     val tutorialId = integer("tutorialId").references(TutorialEntity.id)
     val watchCount = integer("watchCount")
 }
